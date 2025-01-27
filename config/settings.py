23b72011525338928b39169dc2bb82724bc54cf0
dipesh7002 <077bct027.dipesh@pcampus.edu.np>
2025-01-27 23:09:48 +0545

    -----------------------

--- config/settings.py
+++ config/settings.py
@@ -130,10 +130,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'JKR/static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "JKR/static"),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
