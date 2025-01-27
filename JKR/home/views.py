95f8ffc2b6ca09ef3ec26b9a68751e603c193df6
dipesh7002 <077bct027.dipesh@pcampus.edu.np>
2025-01-27 20:38:44 +0545

    aba chalchha hola

--- JKR/home/views.py
+++ JKR/home/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from django.views.generic import TemplateView
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 class HomePage(TemplateView):
-    template_name = 'homes/home.html'
-
+    template_name = os.path.join(BASE_DIR, "index.html") 
 class DocumentationPage(TemplateView):
-    template_name = 'homes/documentation.html'
+    template_name = os.path.join(BASE_DIR, "documentation.html")
 
 
 # class Home(viewsets.Viewset):
